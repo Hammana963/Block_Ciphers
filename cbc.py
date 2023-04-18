946d43f8a56a3ad24364a6ca7136375a7b489be4
Joseph Hammana <Hammana963@gmail.com>
2023-04-17 20:29:33 -0700

    fixed bit size for cbc

--- cbc.py
+++ cbc.py
@@ -4,21 +4,39 @@ from Crypto.Cipher import AES
 
 # Uses PKCS7 to add padding 
 def pad(data):
-    numBlocks = math.ceil(len(data)/float(128))
-    padValue = int(numBlocks * 128 - len(data)) # value determines -> 01, 02 02, 03 03 03, etc..
+    numBlocks = math.ceil(len(data)/float(16))
+    padValue = int(numBlocks * 16 - len(data)) # value determines -> 01, 02 02, 03 03 03, etc..
 
     if padValue == 0:
-        return data + (128).to_bytes() * 128
+        return data + (16).to_bytes() * 16
     else:
         return data + (padValue).to_bytes() * padValue
+
+
+def submit(input):
+    bytestring = pad(input)
+    ciphertext = bytes()
+
+    xoredtext = byte_xor(iv, bytestring[:128])  #first xor with iv
+    encrypttext = cipher.encrypt(xoredtext)     #encrypt text
+    #changing the data of the cipher block before target plaintext block
+    ciphertext += encrypttext                   #add encrypted text to cipher text
+    start = 16
+    #Do CBC encrypt on rest of plaintext using prev encrypt text to xor plaintext
+    for i in range((len(bytestring) // 16)-1):
+        xoredtext = byte_xor(encrypttext, bytestring[start:start+16])   
+        encrypttext = cipher.encrypt(xoredtext)                         
+        start += 16
+        ciphertext += encrypttext
+    return ciphertext    
 
 # XOR function for bytes
 def byte_xor(var, key):
     return bytes(a ^ b for a, b in zip(var, key))
     
-iv = get_random_bytes(32)
+key = get_random_bytes(16)
+iv = get_random_bytes(16)
 
-key = get_random_bytes(32)
 cipher = AES.new(key, AES.MODE_ECB)
 
 with open("mustang.bmp", "rb") as f:
@@ -26,19 +44,11 @@ with open("mustang.bmp", "rb") as f:
 
 #save header of bmp file (128 works on my mac)
 header = bitData[0:128]
-cipherText = bytes()
+rest = bitData[128:]
 
-start = 128 
-cipherText += cipher.encrypt(bitData[start:start+128])
-start += 128
-
-for i in range((len(bitData) // 128) - 2):
-  # XOR sequent blocks with previous ones, encrypt, and add them together
-  cipherText += cipher.encrypt(pad(byte_xor((bitData[start:start+128]), cipherText[start - 128: start])))
-  start += 128
 
 #add header back on
-cipherText = header + cipherText
+cipherText = header + submit(rest)
 
 with open("CBC_encoded_image.bmp", "wb") as f:
   f.write(cipherText)

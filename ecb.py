e609214b660e0e6001adfdc0e15dec8d68aef859
Joseph Hammana <Hammana963@gmail.com>
2023-04-17 20:38:54 -0700

    fixed block size for ebc

--- ecb.py
+++ ecb.py
@@ -4,15 +4,16 @@ from Crypto.Cipher import AES
 
 # Uses PKCS7 to add padding 
 def pad(data):
-    numBlocks = math.ceil(len(data)/float(128))
-    padValue = int(numBlocks * 128 - len(data)) # value determines -> 01, 02 02, 03 03 03, etc..
+    numBlocks = math.ceil(len(data)/float(16))
+    padValue = int(numBlocks * 16 - len(data)) # value determines -> 01, 02 02, 03 03 03, etc..
 
     if padValue == 0:
-        return data + (128).to_bytes() * 128
+        return data + (16).to_bytes() * 16
     else:
         return data + (padValue).to_bytes() * padValue
 
-key = get_random_bytes(32)
+
+key = get_random_bytes(16)
 cipher = AES.new(key, AES.MODE_ECB)
 
 with open("mustang.bmp", "rb") as f:
@@ -20,18 +21,22 @@ with open("mustang.bmp", "rb") as f:
 
 #save header of bmp file (128 works on my mac)
 header = bitData[0:128]
+rest = bitData[128:]
 cipherText = bytes()
 
-start = 128
 
-for i in range((len(bitData) // 128) - 1):
+
+
+start = 16
+bitData = pad(rest)
+for i in range((len(rest) // 16) - 1):
   # encrypt 128 blocks, and add them together
-  cipherText += cipher.encrypt(bitData[start:start+128])
-  start += 128
+  cipherText += cipher.encrypt(rest[start:start+16])
+  start += 16
 
 #pad the final block if it isnt long enough
-if start < len(bitData):
-  cipherText += cipher.encrypt(pad(bitData[start:start+128]))
+if start < len(rest):
+  cipherText += cipher.encrypt(pad(rest[start:start+16]))
 
 #add header back on
 cipherText = header + cipherText
